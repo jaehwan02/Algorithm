39a7ab58804571c89eaaa8a073fda055902b394f
이재환 <163807152+jaehwan02@users.noreply.github.com>
2024-09-20 20:16:41 +0900

    [Silver III] Title: 1로 만들기, Time: 332 ms, Memory: 38932 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,13 @@
+def f(n):
+    dp[1] = 0
+    for i in range(2, n+1):
+        dp[i] = dp[i-1] + 1
+        if i%3==0:
+            dp[i] = min(dp[i//3]+1, dp[i])
+        if i%2==0:
+            dp[i] = min(dp[i//2]+1, dp[i])
+    return dp[n]
+
+n = int(input())
+dp = [-1] * (n + 1)
+print(f(n))
